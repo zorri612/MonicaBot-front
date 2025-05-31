99a34915754df84f3bc4c0b5325dfae56d7602ca
Nicolas <zorrillanico200312@gmail.com>
2025-05-30 23:23:03 -0500

    correcion link router

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 
 function Register() {
   const [form, setForm] = useState({ name: '', email: '', password: '' });
