164a28a6eff07929420bd6755276f130ae5778de
Nicolas <zorrillanico200312@gmail.com>
2025-05-30 21:19:59 -0500

    cambie logica para vercel

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -10,7 +10,7 @@ function Register() {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://monica-bot-back.vercel.app/api/auth/register', form);
+      const res = await axios.post('http://localhost:5000/api/auth/register', form);
       setMessage(res.data.message);
       navigate('/login'); // redirigir al componente de login después de registrarse
     } catch (err) {
